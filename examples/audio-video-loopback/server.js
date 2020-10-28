fc3622b3f9e56602242103e5b2a313fbd759b5be
Your Name <you@example.com>
2020-10-28 14:59:51 +0530

    message

--- examples/audio-video-loopback/server.js
+++ examples/audio-video-loopback/server.js
@@ -1,12 +1,42 @@
 'use strict';
 
+const { PassThrough } = require('stream')
+const fs = require('fs')
+
+const { RTCAudioSink, RTCVideoSink } = require('wrtc').nonstandard;
+
+
 function beforeOffer(peerConnection) {
   const audioTransceiver = peerConnection.addTransceiver('audio');
   const videoTransceiver = peerConnection.addTransceiver('video');
+  const stream = {
+    recordPath: 'test'+ '.mp4',
+    audio: new PassThrough()
+  };
+
+  const onAudioData = ({ samples: { buffer } }) => {
+    if (!stream.end) {
+      stream.audio.push(Buffer.from(buffer));
+    }
+  };
+
+  audioSink.addEventListener('data', onAudioData);
+
+
+  stream.audio.on('end', () => {
+    audioSink.removeEventListener('data', onAudioData);
+  });
+
+  console.log((new StreamInput(stream.audio)).url)
+
+  const audioSink = new RTCAudioSink(audioTransceiver.receiver.track);
+
   return Promise.all([
     audioTransceiver.sender.replaceTrack(audioTransceiver.receiver.track),
     videoTransceiver.sender.replaceTrack(videoTransceiver.receiver.track)
   ]);
 }
+
+
 
 module.exports = { beforeOffer };
